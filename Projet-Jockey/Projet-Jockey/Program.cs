3ec9903851073e7a956e01bc95a4898a9141ec03
Moreira dos Santos Daniel <daniel.moreiradossantos@rpn.ch>
2017-12-12 16:12:27 +0100

    Start projet

---
+++ Projet-Jockey/Projet-Jockey/Program.cs
@@ -0,0 +1,39 @@
+﻿/***************
+ * Projet: Jockey
+ * Auteur: Daniel
+ * Date: 12.12.17
+ * Classe: 1M4I1C
+ * Version c#: visual studio pro 2017
+ * OS: win 10 pro
+ * 
+ * But: Réaliser une course de chevaux
+ *************/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projet_Jockey
+{
+    class Program
+    {
+        const int iArrive = 50;
+        static void Main(string[] args)
+        {
+            int[] tChevaux = new int[] { 0, 0, 0, 0 };
+            bool bfalse = false;
+            Random random = new Random();
+
+
+            
+           
+            
+        }
+    }
+
+
+
+}
+    
+
