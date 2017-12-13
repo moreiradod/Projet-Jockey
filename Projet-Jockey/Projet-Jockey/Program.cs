79108d55096650080e680d400b42e4fa40e41248
Moreira dos Santos Daniel <daniel.moreiradossantos@rpn.ch>
2017-12-13 09:18:56 +0100

    add menu

--- Projet-Jockey/Projet-Jockey/Program.cs
+++ Projet-Jockey/Projet-Jockey/Program.cs
@@ -24,11 +24,27 @@ namespace Projet_Jockey
             int[] tChevaux = new int[] { 0, 0, 0, 0 };
             bool bfalse = false;
             Random random = new Random();
+            string strNom;
+            string strParis;
 
+            Console.WriteLine("Bienvenue à la course des chevaux !!");
+            Console.WriteLine("****************************************");
+            Console.Write("Quel est votre nom ? ");
+            strNom = Console.ReadLine();
 
-            
-           
-            
+            //Console.Write("salut {0}", strNom);
+            Console.WriteLine("\n*       Les paris sont ouverts !       *");
+            Console.WriteLine("****************************************");
+            Console.Write("Sur quel cheval désirez vous misez (1,2,3 ou 4)? ");
+            strParis = Console.ReadLine();
+            Console.WriteLine("****************************************\n");
+            Console.WriteLine("Appuyez de manière répétée sur une touche pour faire avancer les chevaux. ");//Consigne de l'exercice.
+
+            Console.ReadKey();
+            Console.Clear(); //Nettoie la console, dès qu'une touche est pressée. 
+
+            Console.Write("salut {0}", strNom);
+            Console.ReadKey();
         }
     }
 
